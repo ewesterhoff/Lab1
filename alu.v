78d428c1ac5802648fc67aa682ab854d8439cd50
vivienyuwenchen <vivienyuwenchen@gmail.com>
2018-09-29 18:47:55 -0400

    Change definitions

--- alu.v
+++ alu.v
@@ -1,7 +1,10 @@
 // Adder circuit
 `define AND and #50
 `define OR or #50
+`define NAND nand #50
+`define NOR nor #50
 `define XOR xor #50
+`define NOT not #50
 
 module ALU
 (
@@ -14,5 +17,5 @@ input[31:0]   operandB,
 input[2:0]    command
 );
 	// Your code here
-	
+
 endmodule
