cd748cf1a95a8116dff3de50600b43d59f18bfa5
ewesterhoff <emma.westerhoff@students.olin.edu>
2018-10-04 14:34:49 -0400

    SLT minor changes

--- alu_function.v
+++ alu_function.v
@@ -91,11 +91,11 @@ module SLTmod #( parameter n = 31 )
 
    	genvar i;
    	generate for (i = 0; i < 32; i = i + 1) begin
-   			AddSubN adder(.sum(sub[i]), .carryout(carryin0[i+1]), .overflow(overflow), .a(a[i]), .b(b[i]), .carryin(carryin0[i]), .subtract(subtract));
+   			AddSubN adder(.sum(sub[i]), .carryout(carryin0[i+1]), .overflow(overflow0), .a(a[i]), .b(b[i]), .carryin(carryin0[i]), .subtract(subtract));
      end
    	endgenerate
 
-    `XOR SLTXOR(slt[31], sub[n], overflow0);
+    `XOR SLTXOR(slt[0], sub[n], overflow0);
 
     assign carryout = 0;
     assign overflow = 0;
